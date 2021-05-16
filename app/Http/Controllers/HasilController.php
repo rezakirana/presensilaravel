ac54cb137d7c08fe3533ad1e92d1a4ab8d61fe76
resa <resa@resas-MacBook-Pro.local>
2021-05-16 16:18:44 +0700

    finish perhitungan

--- app/Http/Controllers/HasilController.php
+++ app/Http/Controllers/HasilController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\BobotKriteria;
+use App\Model\Kriteria;
+use App\Model\Alternatif;
+use App\Model\SubKriteria;
+use App\Model\NilaiAlternatif;
 use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
+use App\Charts\PenilaianLineChart;
 
 class HasilController extends Controller
 {
@@ -13,7 +21,7 @@ class HasilController extends Controller
      */
     public function index()
     {
-        //
+        // 
     }
 
     /**
@@ -84,6 +92,156 @@ class HasilController extends Controller
 
     public function hasil_perhitungan()
     {
+        $jumBobot = BobotKriteria::where(['id_user' => Auth::id()])->sum('bobot');
+        $step1 = [];
+        $kriteria = Kriteria::join('bobot_kriteria', 'bobot_kriteria.id_kriteria','=','kriteria.id')
+                                ->where(['bobot_kriteria.id_user' => Auth::id()])
+                                ->select([
+                                    'kriteria.id as kriteria_id',
+                                    'kriteria.nama',
+                                    'kriteria.jenis',
+                                    'bobot_kriteria.bobot',
+                                ])->get();
+        $alternatif = Alternatif::all();
+        // normalisasi bobot
+        foreach ($kriteria as $key => $k) {
+            $tmp = 0;
+            $tmp = (int)$k->bobot/(int)$jumBobot;
+            $step1[$k->nama] = $tmp;
+        }
+        // end normalisasi bobot
         
+        $step2 = [];
+        // step 2 rating kecocokan
+        foreach ($alternatif as $key => $value) {
+            $penilaianTmp = Alternatif::join('nilai_alternatif', 'nilai_alternatif.id_alternatif','=','alternatif.id')
+                                ->join('sub_kriteria','sub_kriteria.id','=','nilai_alternatif.id_sub_kriteria')
+                                ->join('kriteria', 'kriteria.id', '=', 'sub_kriteria.id_kriteria')
+                                ->join('bobot_kriteria', 'bobot_kriteria.id_kriteria', '=', 'kriteria.id')
+                                ->where([
+                                        'nilai_alternatif.id_alternatif' => $value->id,
+                                        'bobot_kriteria.id_user' => Auth::id()
+                                        ])
+                                ->select([
+                                    'alternatif.id as id_alternatif',
+                                    'alternatif.nama as nama_alternatif',
+                                    'kriteria.id as kriteria_id',
+                                    'kriteria.jenis as kriteria_jenis',
+                                    'kriteria.nama as nama_kriteria',
+                                    'sub_kriteria.id as sub_kriteria_id',
+                                    'sub_kriteria.nama as nama_subKriteria',
+                                    'sub_kriteria.keterangan as keterangan_subKriteria',
+                                    'sub_kriteria.bobot as bobot_subKriteria',
+                                    'bobot_kriteria.bobot as bobot_kriteria',
+                                ])->get();
+            foreach ($penilaianTmp as $kunci => $nilai) {
+                if(!isset($step2[$key])) {
+                    $step2[$key] = new \StdClass();
+                }
+                // $step2[$key]->id = $nilai->id;
+                $step2[$key]->alternatif_id = $nilai->id_alternatif;
+                $step2[$key]->alternatif_nama = $nilai->nama_alternatif;
+                $step2[$key]->kriteria_id[$kunci] = $nilai->kriteria_id;
+                $step2[$key]->sub_kriteria_id[$kunci] = $nilai->sub_kriteria_id;
+                $step2[$key]->kriteria[$kunci] = $nilai->nama_kriteria;
+                $step2[$key]->namaSubKriteria[$kunci] = $nilai->nama_subKriteria;
+                $step2[$key]->keterangan[$kunci] = $nilai->keterangan_subKriteria;
+                $step2[$key]->sub_bobot[$kunci] = $nilai->bobot_subKriteria;
+                $step2[$key]->bobot[$kunci] = $nilai->bobot_kriteria;
+                $step2[$key]->jenis[$kunci] = $nilai->kriteria_jenis;
+            }
+        }
+        // end step 2
+
+        $subKriteriaData = [];
+        foreach ($kriteria as $key => $value) {
+            $sub = SubKriteria::where('id_kriteria',$value->kriteria_id)->get();
+            foreach ($sub as $key2 => $s) {
+                if(!isset($subKriteriaData[$key])) {
+                    $subKriteriaData[$key] = new \StdClass();
+                }
+                $subKriteriaData[$key]->kriteria = $value->nama;
+                $subKriteriaData[$key]->sub_kriteriaNama[$key2] = $s->nama;  
+                $subKriteriaData[$key]->sub_kriteria[$key2] = $s->keterangan;  
+                $subKriteriaData[$key]->bobot_sub[$key2] = $s->bobot;  
+            }
+        }
+
+        // perhitungan vector S
+        $step3 = [];
+        $jumVectorS = 0;
+        foreach ($step2 as $key => $s) {
+            $remanen2 = null;
+            $tmp = null;
+            $tmp2 = 1;
+            if(!isset($step3[$key])) {
+                $step3[$key] = new \StdClass();
+            }
+            $step3[$key]->alternatif = $s->alternatif_nama;
+            foreach ($s->sub_bobot as $key2 => $nilai) {
+                $remanen = null;
+                if ($s->jenis[$key2] == 'benefit') {
+                    $tmp = pow($nilai, $step1[$s->kriteria[$key2]]);
+                    $remanen = "(".$nilai."^".$step1[$s->kriteria[$key2]].")";
+                } else {
+                    $tmp = pow($nilai,-$step1[$s->kriteria[$key2]]);
+                    $remanen = "(".$nilai."^-".$step1[$s->kriteria[$key2]].")";
+                }
+                $tmp2 = $tmp2 * $tmp;
+                if (is_null($remanen2)) {
+                    $remanen2 = $remanen;
+                } else {
+                    $remanen2 =  $remanen2 . ' * '. $remanen;
+                }      
+            }
+            $step3[$key]->perhitungan = $remanen2;
+            $step3[$key]->nilai = $tmp2;
+            $jumVectorS+= $tmp2;
+        }
+        // end perhitungan vector s
+
+        // perhitungan vector V
+        $step4 = [];
+        foreach ($step3 as $key => $value) {
+            if(!isset($step4[$key])) {
+                $step4[$key] = new \StdClass();
+            }
+            $step4[$key]->nilai = $value->nilai/$jumVectorS;
+            $step4[$key]->perhitungan = $value->nilai.' / '.$jumVectorS;
+            $step4[$key]->alternatif = $value->alternatif;
+        }
+        // end perhitungan vector V
+        
+        // perankingan
+        $sort = collect($step4);
+        $ranking = $sort->sortByDesc('nilai');
+        $label = [];
+        $dataset = [];
+        foreach ($ranking as $key => $value) {
+            array_push($label, $value->alternatif);
+            array_push($dataset, $value->nilai);
+        }
+
+        $penilaianChart = new PenilaianLineChart;
+        $penilaianChart->labels($label);
+        $penilaianChart->dataset('Hasil Penilaian', 'line', $dataset)
+                         ->color("rgb(255, 99, 132)")
+                        ->backgroundcolor("rgb(255, 99, 132)");
+        // end perangkingan
+
+        return view('nilai_alternatif.nilai', [
+            'kriteria' => $kriteria,
+            'subKriteriaData' => $subKriteriaData,
+            'step1' => $step1,
+            'step2' => $step2,
+            'step3' => $step3,
+            'step4' => $step4,
+            'ranking' => $ranking,
+            'jumBobot' => $jumBobot,
+            'jumVectorS' => $jumVectorS,
+            'penilaianChart' => $penilaianChart,
+            'label' => json_encode($label),
+            'dataset' => json_encode($dataset,JSON_NUMERIC_CHECK)
+        ]);
     }
 }
---
+++ resources/views/nilai_alternatif/nilai.blade.php
@@ -0,0 +1,289 @@
+@extends('layouts/main-admin')
+
+@section('title', 'Hasil Perhitungan')
+
+@section('container')
+<script src="https://raw.githubusercontent.com/nnnick/Chart.js/master/dist/Chart.bundle.js"></script>
+<script>
+    var year = <?php echo $label; ?>;
+    console.log(year);
+    var data_click = <?php echo $dataset; ?>;
+    var barChartData = {
+        labels: year,
+        datasets: [{
+            label: 'Alternatif',
+            color: "rgb(255, 99, 132)",
+            backgroundColor: "rgb(255, 99, 132)",
+            data: data_click
+        }]
+    };
+    window.onload = function() {
+        var ctx = document.getElementById("canvas").getContext("2d");
+        window.myBar = new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                elements: {
+                    rectangle: {
+                        borderWidth: 2,
+                        borderColor: 'rgb(0, 255, 0)',
+                        borderSkipped: 'bottom'
+                    }
+                },
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Grafik Hasil Penilaian'
+                }
+            }
+        });
+    };
+</script>
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h2 class="m-0 text-dark">HASIL PERHITUNGAN</h2>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item active">Hasil Perhitungan</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="container-fluid">
+    <div class="card">
+        @include ('includes.flash')
+        <div class="card-body">
+            <!-- perhitungan -->
+            <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            {{-- content 1 --}}
+            <div class="x_panel">
+              <div class="x_title">
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_title">
+                <h2>Data Kriteria</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="item_table">
+                      <tr>
+                        <th style="vertical-align: middle;text-align: center;">Kriteria</th>
+                        <th style="vertical-align: middle;text-align: center;">Bobot Kriteria</th>
+                        <th style="vertical-align: middle;text-align: center;">Jenis Kriteria</th>
+                      </tr>
+                      @foreach ($kriteria as $key => $s)
+                        <tr>
+                            <td>{{ $s->nama }}</td>
+                            <td style="text-align: center;">{{ $s->bobot }}</td>
+                            <td style="text-align: center;">{{$s->jenis}}</td>
+                        </tr>
+                      @endforeach
+                    </table>
+                  </div>
+              </div>
+            </div>
+            {{-- end content 1 --}}
+            {{-- content 2 --}}
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>Data</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="item_table">
+                      <tr>
+                        <th style="vertical-align: middle;text-align: center;">Alternatif</th>
+                        @foreach ($kriteria as $key => $k)
+                          <th style="vertical-align: middle;text-align: center;">{{ $k->nama }}</th>
+                        @endforeach
+                      </tr>
+                      @foreach ($step2 as $key => $s)
+                        <tr>
+                          <td style="text-align: center;">{{ $s->alternatif_nama }}</td>
+                          @for ($i = 0; $i <count($kriteria) ; $i++)
+                            <td style="text-align: center;">{{ $s->namaSubKriteria[$i] }}</td>
+                          @endfor
+                        </tr>
+                      @endforeach
+                    </table>
+                  </div>
+              </div>
+            </div>
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>Data Rating Kecocokan</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="item_table">
+                      <tr>
+                        <th style="vertical-align: middle;text-align: center;">Alternatif</th>
+                        @foreach ($kriteria as $key => $k)
+                          <th style="vertical-align: middle;text-align: center;">{{ $k->nama }}</th>
+                        @endforeach
+                      </tr>
+                      @foreach ($step2 as $key => $s)
+                        <tr>
+                          <td style="text-align: center;">{{ $s->alternatif_nama }}</td>
+                          @for ($i = 0; $i <count($kriteria) ; $i++)
+                            <td style="text-align: center;">{{ $s->sub_bobot[$i] }}</td>
+                          @endfor
+                        </tr>
+                      @endforeach
+                    </table>
+                  </div>
+              </div>
+            </div>
+            {{-- end content 2 --}}
+            {{-- content 3 --}}
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>Normalisasi bobot kriteria</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="item_table">
+                      <tr>
+                        <th style="vertical-align: middle;text-align: center;">No</th>
+                        <th style="vertical-align: middle;text-align: center;">Kriteria</th>
+                        <th style="vertical-align: middle;text-align: center;">Bobot Kriteria</th>
+                        <th style="vertical-align: middle;text-align: center;">Normalisasi Bobot</th>
+                        <th style="vertical-align: middle;text-align: center;">Bobot Ternormalisasi</th>
+                      </tr>
+                      @foreach ($kriteria as $key => $k)
+                        <tr>
+                          <td style="text-align: center;">{{ $key+1 }}</td>
+                          <td style="text-align: center;">{{ $k->kriteria }}</td>
+                          <td style="text-align: center;">{{ $k->bobot }}</td>
+                          <td style="text-align: center;">{{ $k->bobot }} / {{ $jumBobot }}</td>
+                          <td style="text-align: center;">{{ $step1[$k->nama] }}</td>
+                        </tr>
+                      @endforeach
+                      <tr>
+                        <td style="text-align: center;" colspan="2"><b>Total</b></td>
+                        <td style="text-align: center;"><b>{{ $jumBobot }}</b></td>
+                        <td style="text-align: center;"></td>
+                        <td style="text-align: center;"></td>
+                      </tr>
+                    </table>
+                  </div>
+              </div>
+            </div>
+            {{-- end content 3 --}}
+            {{-- content 4 --}}
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>Nilai Vector S pada setiap alternatif</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="item_table">
+                      <tr>
+                        <th style="vertical-align: middle;text-align: center;">No</th>
+                        <th style="vertical-align: middle;text-align: center;">Alternatif</th>
+                        <th style="vertical-align: middle;text-align: center;">Perhitungan</th>
+                        <th style="vertical-align: middle;text-align: center;">Nilai Preferensi</th>
+                      </tr>
+                      @foreach ($step3 as $key => $k)
+                        <tr>
+                          <td style="text-align: center;">{{ $key+1 }}</td>
+                          <td style="text-align: center;">{{ $k->alternatif }}</td>
+                          <td style="text-align: center;">{{ $k->perhitungan }}</td>
+                          <td style="text-align: center;">{{ $k->nilai }}</td>
+                        </tr>
+                      @endforeach
+                      <tr>
+                        <td style="text-align: center;" colspan="3"><b>Total</b></td>
+                        <td style="text-align: center;"><b>{{ $jumVectorS }}</b></td>
+                      </tr>
+                    </table>
+                  </div>
+              </div>
+            </div>
+            {{-- end content 4 --}}
+            {{-- content 5 --}}
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>Nilai Vector V pada setiap alternatif</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="item_table">
+                      <tr>
+                        <th style="vertical-align: middle;text-align: center;">No</th>
+                        <th style="vertical-align: middle;text-align: center;">Alternatif</th>
+                        <th style="vertical-align: middle;text-align: center;">Perhitungan</th>
+                        <th style="vertical-align: middle;text-align: center;">Nilai Preferensi</th>
+                      </tr>
+                      @foreach ($step4 as $key => $k)
+                        <tr>
+                          <td style="text-align: center;">{{ $key+1 }}</td>
+                          <td style="text-align: center;">{{ $k->alternatif }}</td>
+                          <td style="text-align: center;">{{ $k->perhitungan }}</td>
+                          <td style="text-align: center;">{{ $k->nilai }}</td>
+                        </tr>
+                      @endforeach
+                    </table>
+                  </div>
+              </div>
+            </div>
+            {{-- end content 5 --}}
+            {{-- content 6 --}}
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>Perangkingan dan menurutkan nilai Vector V dari yang terbesar ke terkecil</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="item_table">
+                      <tr>
+                        <th style="vertical-align: middle;text-align: center;">No</th>
+                        <th style="vertical-align: middle;text-align: center;">Alternatif</th>
+                        <th style="vertical-align: middle;text-align: center;">Nilai Preferensi</th>
+                      </tr>
+                      @foreach ($ranking as $key => $k)
+                        <tr>
+                          <td style="text-align: center;">{{ $key+1 }}</td>
+                          <td style="text-align: center;">{{ $k->alternatif }}</td>
+                          <td style="text-align: center;">{{ $k->nilai }}</td>
+                        </tr>
+                      @endforeach
+                    </table>
+                  </div>
+              </div>
+            </div>
+            {{-- end content 6 --}}
+            {{-- content 7 --}}
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>Grafik Hasil Penilaian</h2>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="table-responsive">
+                  <canvas id="canvas" height="280" width="600"></canvas>
+                  </div>
+              </div>
+            </div>
+            {{-- end content 7 --}}
+          </div>
+        </div>
+        </div>
+    </div>
+</section>
+@include ('includes.scripts')
+@endsection
